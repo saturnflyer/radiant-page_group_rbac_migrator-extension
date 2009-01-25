ca5ff6702ced0024f7688ea6eb2018560f8fd12a
Jim Gay <jim@saturnflyer.com>
2009-01-25 12:40:52 -0500

    correct documentation and requirement checking

--- page_group_rbac_migrator_extension.rb
+++ page_group_rbac_migrator_extension.rb
@@ -3,13 +3,13 @@
 
 class PageGroupRbacMigratorExtension < Radiant::Extension
   version "1.0"
-  description "Provides migrations to move from PageGroupPermissionsExtension to RbacBaseExtension and RbacPageEditExtension"
+  description "Provides migrations to move from PageGroupPermissionsExtension to RbacPageEditExtension and RbacPageEditExtension"
   url "http://saturnflyer.com/"
   
   class MissingRequirement < StandardError; end
   
   def activate
-    raise PageGroupRbacMigratorExtension::MissingRequirement.new('RbacBaseExtension must be installed and loaded first.') unless defined?(RbacBaseExtension) && defined?(PageGroupRbacMigratorExtension)
+    raise PageGroupRbacMigratorExtension::MissingRequirement.new('RbacPageEditExtension and PageGroupPermissionsExtension must be installed and loaded first.') unless defined?(RbacPageEditExtension) && defined?(PageGroupPermissionsExtension)
   end
   
 end
